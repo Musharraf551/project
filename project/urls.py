2d5e6d2e2a7ba5e196b55e822db8a945e42c7888
musharraf <musharrafaleem2@gmail.com>
2025-02-22 23:07:38 +0530

    imported important stuffs in urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from app.views import *
+from app import views
+
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
