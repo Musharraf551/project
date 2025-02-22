89cc77ce4f9c5a5579cbc983d498a15346e7e93b
musharraf <musharrafaleem2@gmail.com>
2025-02-22 22:59:11 +0530

    imported necessary stuffs in views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from app.models import BookTable, AboutUs, Feedback, ItemList, Items
+from app.models import BookTable
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import BookTable
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 # Create your views here.
