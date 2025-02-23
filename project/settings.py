13fea74651abf9f47a7ac46338f0c83b4523f58a
musharraf <musharrafaleem2@gmail.com>
2025-02-23 23:03:46 +0530

    provide LOGIN_REDIRECT_URL in settings.py

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,8 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_REDIRECT_URL = 'Book_Table'  # Redirect to the dashboard after successful login
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
