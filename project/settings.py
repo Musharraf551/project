ee5ee0dcdc9af364a710b68765f0a46c030dac2d
musharraf <musharrafaleem2@gmail.com>
2025-02-23 22:57:15 +0530

    provided path for staticfiles

--- project/settings.py
+++ project/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [STATIC_DIR]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
