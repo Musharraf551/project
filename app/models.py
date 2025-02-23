59a0f8c291c161c7a3d685aa22e93050b174e1ce
musharraf <musharrafaleem2@gmail.com>
2025-02-23 22:38:37 +0530

    defined Items model in models.py

--- app/models.py
+++ app/models.py
@@ -8,3 +8,13 @@ class ItemList(models.Model):
     
     def __str__(self):
         return self.Category_name
+    
+class Items(models.Model):
+    Item_name = models.CharField(max_length=40)
+    description = models.TextField(blank=False)
+    Price = models.IntegerField()
+    Category = models.ForeignKey(ItemList, related_name='Name', on_delete=models.CASCADE)
+    Image = models.ImageField(upload_to='items/')
+    
+    def __str__(self):
+        return self.Item_name
