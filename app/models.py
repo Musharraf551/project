76c26584767c03885672a9ef8306175d6dac1d8c
musharraf <musharrafaleem2@gmail.com>
2025-02-23 22:33:31 +0530

    defined ItemList model in models.py

--- app/models.py
+++ app/models.py
@@ -3,3 +3,8 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class ItemList(models.Model):
+    Category_name = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.Category_name
