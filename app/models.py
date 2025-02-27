72047aa19f281086c5c8ff1dae847ec0d2b25018
musharraf <musharrafaleem2@gmail.com>
2025-02-27 21:30:06 +0530

    created CartItem model

--- app/models.py
+++ app/models.py
@@ -59,3 +59,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
