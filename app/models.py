7a05ba7c2e13ba34fd595c8701c021911ec3cbe8
musharraf <musharrafaleem2@gmail.com>
2025-02-27 21:29:28 +0530

    created Product model

--- app/models.py
+++ app/models.py
@@ -50,3 +50,12 @@ class BookTable(models.Model):
 
     def __str__(self):
         return f"Booking by {self.user.username} on {self.booking_data}"
+#----------------------------------------------   
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="products/")
+
+    def __str__(self):
+        return self.name
