e7e0891e03e3f161923f12940363bca7f81a7d36
musharraf <musharrafaleem2@gmail.com>
2025-03-13 11:36:51 +0530

    change in models

--- app/models.py
+++ app/models.py
@@ -51,21 +51,4 @@ class BookTable(models.Model):
     def __str__(self):
         return f"Booking by {self.user.username} on {self.booking_data}"
 #----------------------------------------------   
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="products/")
 
-    def __str__(self):
-        return self.name
-class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-
-    def total_price(self):
-        return self.product.price * self.quantity
-
-    def __str__(self):
-        return f"{self.quantity} x {self.product.name}"
